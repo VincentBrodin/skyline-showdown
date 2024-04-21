56723f65a6676737e135f89df1b8c40d1f4920b7
Vincent Brodin <vincent.brodin21@gmail.com>
2024-04-21 22:10:41 +0200

    Update NetworkAudio.cs

--- Assets/Code/Networking/NetworkAudio.cs
+++ Assets/Code/Networking/NetworkAudio.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Mirror;
+﻿using Mirror;
 using UnityEngine;
 
 namespace Code.Networking{
@@ -16,6 +15,9 @@ namespace Code.Networking{
             pitch = audioSource.pitch;
         }
 
+        /// <summary>
+        /// Syncs the Play function from AudioSource to all clients
+        /// </summary>
         public void Play(){
             CmdPlay();
         }
@@ -31,7 +33,28 @@ namespace Code.Networking{
             audioSource.volume = volume;
             audioSource.Play();
         }
+
+        /// <summary>
+        /// Syncs the Stop function from AudioSource to all clients
+        /// </summary>
+        public void Stop(){
+            CmdStop();
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdStop(){
+            ClientStop();
+        }
+
+        [ClientRpc]
+        private void ClientStop(){
+            audioSource.Stop();
+        }
         
+        /// <summary>
+        /// Syncs the PlayDelay function from AudioSource to all clients
+        /// </summary>
+        /// <param name="delay">The given delay</param>
         public void PlayDelay(float delay){
             CmdPlayDelay(delay);
         }
@@ -46,6 +69,34 @@ namespace Code.Networking{
             audioSource.pitch = pitch;
             audioSource.volume = volume;
             audioSource.PlayDelayed(delay);
+        }
+        
+        /// <summary>
+        /// Syncs the volume between clients
+        /// </summary>
+        /// <param name="newValue">The new volume (0,1)</param>
+        public void SetVolume(float newValue){
+            CmdSetVolume(newValue);
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdSetVolume(float newValue){
+            newValue = Mathf.Clamp(newValue, 0, 1);
+            volume = newValue;
+        }
+        
+        /// <summary>
+        /// Syncs the pitch between clients
+        /// </summary>
+        /// <param name="newValue">The new pitch (-3,3)</param>
+        public void SetPitch(float newValue){
+            CmdSetPitch(newValue);
+        }
+
+        [Command(requiresAuthority = false)]
+        private void CmdSetPitch(float newValue){
+            newValue = Mathf.Clamp(newValue, -3, 3);
+            pitch = newValue;
         }
     }
 }
