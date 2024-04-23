d0eab8f128c950cb02101bd0b7ce04cd77c9715a
Vincent Brodin <vincent.brodin21@gmail.com>
2024-04-24 01:23:07 +0200

    Added some QOL

---
+++ Assets/Code/Interactions/InteractiveRadio.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Code.Interactions{
+    public class InteractiveRadio : NetworkBehaviour{
+        [SyncVar(hook = nameof(OnSwitch))] public int currentTrack;
+        public AudioSource audioSource;
+        public AudioClip[] tracks;
+        public Interactive interactive;
+
+        private void Start(){
+            interactive.OnInteraction.AddListener(Switch);
+        }
+
+        private void Switch(){
+            ServerSwitch();
+        }
+
+        [Command(requiresAuthority = false)]
+        private void ServerSwitch(){
+            if (currentTrack + 1 > tracks.Length - 1){
+                currentTrack = 0;
+            }
+            else{
+                currentTrack += 1;
+            }
+        }
+
+        private void OnSwitch(int oldValue, int newValue){
+            audioSource.Stop();
+            audioSource.clip = tracks[newValue];
+            audioSource.PlayDelayed(0.5f);
+        }
+
+        private void OnValidate(){
+            if (interactive != null) return;
+            Interactive onGameObject = GetComponent<Interactive>();
+            Interactive onChild = GetComponentInChildren<Interactive>();
+
+            if (onGameObject){
+                interactive = onGameObject;
+            }
+            else if (onChild){
+                interactive = onChild;
+            }
+        }
+    }
+}
