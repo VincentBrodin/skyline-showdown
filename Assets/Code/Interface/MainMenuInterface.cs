c95bb4a5acaf596c6e91990547ae0d624fb5a8e1
Vincent Brodin <vincent.brodin21@gmail.com>
2024-04-23 18:39:26 +0200

    Added ip and port to network manager

--- Assets/Code/Interface/MainMenuInterface.cs
+++ Assets/Code/Interface/MainMenuInterface.cs
@@ -2,30 +2,32 @@
 using Code.Interface.Settings;
 using Code.Managers;
 using Code.Networking;
+using kcp2k;
 using Mirror;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Code.Interface{
     public class MainMenuInterface : MonoBehaviour{
-        [Header("Main")] 
-        public Button playWithFriendsButton;
+        [Header("Main")] public Button playWithFriendsButton;
         public Button joinRandomButton;
         public Button optionButton;
         public Button creditsButton;
         public Button quitButton;
 
-        [Header("Host")] 
-        public GameObject hostScreen;
+        [Header("Host")] public GameObject hostScreen;
         public Button hostGameButton;
         public Button joinLanGameButton;
         public Button backFromHostButton;
+        public TMP_InputField ipAddress;
+        public TMP_InputField port;
 
-        [Header("Error")] 
-        public GameObject errorScreen;
+        [Header("Error")] public GameObject errorScreen;
         public Button closeError;
 
         private CustomNetworkManager _manager;
+
         private CustomNetworkManager Manager(){
             if (_manager == null)
                 _manager = NetworkManager.singleton as CustomNetworkManager;
@@ -37,18 +39,21 @@ namespace Code.Interface{
             playWithFriendsButton.onClick.AddListener(OpenHost);
             optionButton.onClick.AddListener(SettingsMenu.Singleton.Show);
             quitButton.onClick.AddListener(Quit);
-            
+
             hostGameButton.onClick.AddListener(HostGame);
             joinLanGameButton.onClick.AddListener(JoinLanGame);
             backFromHostButton.onClick.AddListener(CloseHost);
             hostScreen.SetActive(false);
 
             Manager().transport.OnClientDisconnected += ClientError;
-            
+
             closeError.onClick.AddListener(CloseError);
             errorScreen.SetActive(false);
-            
+
             CursorManager.Singleton.ResetShow();
+
+            ipAddress.text = Manager().networkAddress;
+            port.text = $"{Manager().GetComponent<KcpTransport>().port}";
         }
 
         private void CloseError(){
@@ -57,7 +62,6 @@ namespace Code.Interface{
 
         private void OpenHost(){
             hostScreen.SetActive(true);
-            
         }
 
         private void CloseHost(){
@@ -66,25 +70,31 @@ namespace Code.Interface{
 
         private void HostGame(){
             Debug.Log("Starting Host");
+            if (!ushort.TryParse(port.text, out ushort newPort)) return;
+            Manager().networkAddress = ipAddress.text;
+            Manager().GetComponent<KcpTransport>().port = newPort;
             NetworkManager.singleton.StartHost();
         }
 
         private void JoinLanGame(){
             Debug.Log("Starting Lan");
+            if (!ushort.TryParse(port.text, out ushort newPort)) return;
+            Manager().networkAddress = ipAddress.text;
+            Manager().GetComponent<KcpTransport>().port = newPort;
             NetworkManager.singleton.StartClient();
         }
-        
+
         private void Quit(){
             Application.Quit();
         }
 
         private void ClientError(){
-            if(errorScreen)
+            if (errorScreen)
                 errorScreen.SetActive(true);
         }
 
         private void OnDestroy(){
-            if(Manager())
+            if (Manager())
                 Manager().transport.OnClientDisconnected += ClientError;
         }
     }
