e00df3da6667a771aa533c9a7b209d9e32bfb946
Vincent Brodin <vincent.brodin21@gmail.com>
2024-04-27 22:17:46 +0200

    Some nice improvements to bounce pads

--- Assets/Code/MapTools/BouncePad.cs
+++ Assets/Code/MapTools/BouncePad.cs
@@ -1,16 +1,19 @@
-﻿using Code.Players;
+﻿using Code.Networking;
+using Code.Players;
 using UnityEngine;
 
 namespace Code.MapTools{
     public class BouncePad : MonoBehaviour{
         public float upForce;
         public float directionalForce;
+        public new NetworkAudio audio;
 
         private void OnTriggerEnter(Collider other){
             if (!other.transform.parent) return;
             if (!other.transform.parent.TryGetComponent(out GamePlayer gamePlayer)) return;
             if (!gamePlayer.isLocalPlayer) return;
 
+            audio.Play();
             Rigidbody rb = gamePlayer.GetComponent<Rigidbody>();
             Vector3 velocity = rb.velocity;
             velocity.y = 0;
