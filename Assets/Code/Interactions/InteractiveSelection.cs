32715680c0ff9a58163ab048965fec1f385d5085
Vincent Brodin <vincent.brodin21@gmail.com>
2024-04-28 16:02:47 +0200

    Custom settings start

---
+++ Assets/Code/Interactions/InteractiveSelection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Interactions{
+    public class InteractiveSelection : MonoBehaviour{
+        public Interactive interactive;
+
+        public List<int> validIndex;
+        public Selections selections;
+
+        private void FixedUpdate(){
+            if (validIndex.Contains(selections.currentSelected)){
+                if (!interactive.active)
+                    interactive.SetActive(true);
+            }
+            else{
+                if (interactive.active)
+                    interactive.SetActive(false);
+            }
+        }
+
+        protected void OnValidate(){
+            if (interactive != null) return;
+            Interactive onGameObject = GetComponent<Interactive>();
+            Interactive onChild = GetComponentInChildren<Interactive>();
+
+            if (onGameObject){
+                interactive = onGameObject;
+            }
+            else if (onChild){
+                interactive = onChild;
+            }
+        }
+    }
+}
